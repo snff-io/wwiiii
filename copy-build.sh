7ccf8719500cba93395b598ef6b9c0e7ef1a5e03
Ubuntu <zaja@machine.xrbmnwxjuluuhmt5rd1nrkw4df.bx.internal.cloudapp.net>
2024-02-06 20:54:14 +0000

    uhhh

--- copy-build.sh
+++ copy-build.sh
@@ -1,10 +1,13 @@
-cp _build/bbs/bbs /opt/wwiv
-cp _build/wwivd/wwivd /opt/wwiv
-cp _build/wwivconfig/wwivconfig /opt/wwiv
-cp _build/network/network /opt/wwiv
-cp _build/network/network /opt/wwiv
-cp _build/network1/network1 /opt/wwiv
-cp _build/network3/network3 /opt/wwiv
-cp _build/networkb/networkb /opt/wwiv
-cp _build/networkc/networkc /opt/wwiv
-cp _build/networkt/networkt /opt/wwiv
+cp _build/bbs/bbs /opt/bbs.worldcomputer.info/wwiv
+cp _build/wwivd/wwivd /opt/bbs.worldcomputer.info/wwiv
+cp _build/wwivconfig/wwivconfig /opt/bbs.worldcomputer.info/wwiv
+cp _build/network/network /opt/bbs.worldcomputer.info/wwiv
+cp _build/network/network /opt/bbs.worldcomputer.info/wwiv
+cp _build/network1/network1 /opt/bbs.worldcomputer.info/wwiv
+cp _build/network3/network3 /opt/bbs.worldcomputer.info/wwiv
+cp _build/networkb/networkb /opt/bbs.worldcomputer.info/wwiv
+cp _build/networkc/networkc /opt/bbs.worldcomputer.info/wwiv
+cp _build/networkt/networkt /opt/bbs.worldcomputer.info/wwiv
+cp welcome.msg /opt/bbs.worldcomputer.info/wwiv/gfiles/welcome.msg
+cp welcome.msg /opt/bbs.worldcomputer.info/wwiv/gfiles/welcome.ans
+
